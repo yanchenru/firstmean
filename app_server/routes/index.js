1e41a26a368ce717ec8b4d739202d7cb4a12692b
Chenru Yan <yanchenru@gmail.com>
2016-04-22 16:55:21 -0600

    add controller

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
