04764f74edd4187589a8a820a876332ebc72dca3
Chenru Yan <yanchenru@gmail.com>
2016-04-27 14:39:00 -0600

    running version

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
